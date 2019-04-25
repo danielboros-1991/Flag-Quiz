ac7adb8521aaadf3512df63a81ea17abd0e24722
Daniel Boros <daniel.boros@online.de>
2019-04-25 15:15:24 +0000

    We now are making a version which wants to put all logic into the ejs file for the search page, next up we are using the :id (Restful Routes)

--- app.js
+++ app.js
@@ -5,23 +5,32 @@ app.use(express.static("public"));
 app.use(express.static("public/images"));
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
+var fs = require('fs');
 
-var searched = "";
+var data = {};
+
+// var searched = "";
+
+var json = JSON.parse(fs.readFileSync('public/flags.json', 'utf8'));
+for(key in json){
+    data[key]=json[key]
+}
+console.log(data)
 
 app.get("/", function(req,res){
     res.render("quiz")
 });
 
 app.get("/search", function(req,res){
-    res.render("search",{searched:searched})
+    res.render("search",{data:data})
 });
 
-app.post("/search", function(req,res){
-    var search = req.body.searchImage;
-    searched = search
-    res.redirect("/search");
+// app.post("/search", function(req,res){
+//     var search = req.body.searchImage;
+//     searched = search
+//     res.redirect("/search");
     
-});
+// });
 
 
 
