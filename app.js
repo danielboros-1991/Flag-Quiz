b9a9c215efd79629acf55ef1d9bc5a8031483807
Daniel Boros <daniel.boros@online.de>
2019-04-27 10:45:28 +0000

    Cleaned up code a bit more, should be finished to upload to repositories altough I still need to check if I should leave comments or not

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-var express = require("express");
+var express    = require("express");
+    bodyParser = require("body-parser");
+    fs         = require('fs');
+    
 var app = express();
-app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("public/images"));
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
-var fs = require('fs');
+app.set("view engine", "ejs");
 
 var data = {};
 var json = JSON.parse(fs.readFileSync('public/flags.json', 'utf8'));
 for(key in json){
-    data[key]=json[key]
+    data[key]=json[key];
 }
 
 app.get("/", function(req,res){
     res.render("quiz")
 });
 
+
 app.get("/search", function(req,res){
     res.render("search",{data:data})
+});
+
+app.post("/search", function(req,res){
+    var search = req.body.searchInput;
+    res.redirect("/search/"+search);
 });
 
 app.get("/search/:id", function(req,res){
     var id = req.params.id;
     res.render("show",{data:data, id:id})
 });
-
-
-
-app.post("/search", function(req,res){
-    var search = req.body.searchImage;
-    res.redirect("/search/"+search);
-    
-});
-
-
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
--- public/quiz.js
+++ public/quiz.js
@@ -2,102 +2,75 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var x = JSON.parse(this.responseText);
-    var images = []
-    var lyrics = []
+    var images = [];
+    var countryNames = [];
     for (var key in x) {
-        if (x.hasOwnProperty(key)) {
-            images.push(x[key])
-            lyrics.push(key)
-            
-            // console.log(key + " -> " + list[key]);
-            
+        if (x.hasOwnProperty(key)){
+            images.push(x[key]);
+            countryNames.push(key);
         }
     }
     
-    console.log(images)
-    
-    
-	 //var lyrics = ["Andorra",
-	 //"United Arab Emirates",
-	 //"Afghanistan",
-	 //"Antigua and Barbuda", 
-	 //"Albania",
-	 //"Hungary",
-	 //"Central African Republic",
-	 //"Djibouti",
-	 //"Sao Tome and Principe",
-	 //"Togo",
-	 //"Uzbekistan",
-	 //"France"];
-	
 	// Here all squares, which are the containers with the images in them, are selected:
 	
 	var square = document.querySelectorAll(".squares");
 	var resetButton = document.querySelector("#reset");
-	var lyricsDisplay = document.querySelector("#lyrics");
+	var countryNameDisplay = document.querySelector("#countryName");
 	var resultDisplay = document.querySelector("#result");
 	var hardModeButton = document.querySelector("#hard");
 	var mediumModeButton = document.querySelector("#medium");
 	var easyModeButton = document.querySelector("#easy");
-	
 	var imageNumber = images.length;
+	var selectedMode = "medium";
 	
-	var select = "medium"
-	
-	defaultMode()
+	defaultMode();
 	
 	easyModeButton.addEventListener("click", function(){
 		easyModeButton.classList.add("selected");
-		
-		if(select == "medium"){
+		if(selectedMode == "medium"){
 			mediumModeButton.classList.remove("selected");
-		} else if(select == "hard"){
+		} else if(selectedMode == "hard"){
 			hardModeButton.classList.remove("selected");
 		}
-	
-		select = "easy"
-		
-		
+		selectedMode = "easy";
 		imagesDisplayed = 3;
 		randomArray = random(imageNumber,imagesDisplayed);
-		displayedFlags=[]
+		displayedFlags=[];
 		for(var i = 0; i < square.length; i++){
 			if(i < imagesDisplayed){
-				var number = randomArray[i]
-				displayedFlags.push(number)
-				var a = "url("+ images[number] +")"
+				var number = randomArray[i];
+				displayedFlags.push(number);
+				var a = "url("+ images[number] +")";
 				square[i].style.backgroundImage = a;
 			} else{
 				square[i].style.display = "none";
 			}
 		} 
 		num = pickNumber(displayedFlags);
-		lyricsDisplay.textContent = lyrics[num];
+		countryNameDisplay.textContent = countryNames[num];
 	});
 	
 	hardModeButton.addEventListener("click", function(){
 		hardModeButton.classList.add("selected");
-		if(select == "medium"){
+		if(selectedMode == "medium"){
 			mediumModeButton.classList.remove("selected");
-		} else if(select == "easy"){
+		} else if(selectedMode == "easy"){
 			easyModeButton.classList.remove("selected");
 		}
-	
-		select = "hard"
+		selectedMode = "hard";
 		if(imagesDisplayed === 6){
 			mediumModeButton.classList.remove("selected");
 		} else{
 			easyModeButton.classList.remove("selected");
 		}
-		
 		imagesDisplayed = 9;
 		randomArray = random(imageNumber,imagesDisplayed);
-		displayedFlags=[]
+		displayedFlags=[];
 		for(var i = 0; i < square.length; i++){
 			if(i < imagesDisplayed){
-				var number = randomArray[i]
-				displayedFlags.push(number)
-				var a = "url("+ images[number] +")"
+				var number = randomArray[i];
+				displayedFlags.push(number);
+				var a = "url("+ images[number] +")";
 				square[i].style.backgroundImage = a;
 				square[i].style.display = "block";
 			} else{
@@ -105,7 +78,7 @@ xmlhttp.onreadystatechange = function() {
 			}
 		} 
 		num = pickNumber(displayedFlags);
-		lyricsDisplay.textContent = lyrics[num];
+		countryNameDisplay.textContent = countryNames[num];
 	});
 	
 	mediumModeButton.addEventListener("click", function(){
@@ -116,45 +89,40 @@ xmlhttp.onreadystatechange = function() {
 	// reached the length of the images array.
 	
 	function defaultMode(){
-		
-		
 		mediumModeButton.classList.add("selected");
-		if(select == "hard"){
+		if(selectedMode == "hard"){
 			hardModeButton.classList.remove("selected");
-		} else if(select == "easy"){
+		} else if(selectedMode == "easy"){
 			easyModeButton.classList.remove("selected");
 		}
-	
-		select = "medium"
-		
+		selectedMode = "medium";
 		imagesDisplayed = 6;
 		randomArray = random(imageNumber,imagesDisplayed);
-		displayedFlags=[]
+		displayedFlags=[];
 		for(var i = 0; i < square.length; i++){
 			if(i < imagesDisplayed){
-				var number = randomArray[i]
-				displayedFlags.push(number)
-				var a = "url("+ images[number] +")"
+				var number = randomArray[i];
+				displayedFlags.push(number);
+				var a = "url("+ images[number] +")";
 				square[i].style.backgroundImage = a;
 				square[i].style.display = "block";
-				
 			} else{
 				square[i].style.display = "none";
 			}
 		} 
 		num = pickNumber(displayedFlags);
-		lyricsDisplay.textContent = lyrics[num];
+		countryNameDisplay.textContent = countryNames[num];
 	}
 	
 	function random(imageNumber, imagesDisplayed){
-		var randomSet = new Set()
+		var randomSet = new Set();
 		while(randomSet.size < imagesDisplayed){
 			var randomNumber = Math.floor((Math.random() * imageNumber));
-			randomSet.add(randomNumber)
+			randomSet.add(randomNumber);
 			}
 		// Then we turn the set into an array and we have an array which every time the page gets refreshed creates a new arbitrary
 		// sequence of the needed numbers to assign the squares in arbitrary sequence to the images. 
-		var randomArray = Array.from(randomSet)
+		var randomArray = Array.from(randomSet);
 		return randomArray;
 	}
 	
@@ -163,8 +131,8 @@ xmlhttp.onreadystatechange = function() {
 	
 	function pickNumber(randomArray){
 		var i = Math.floor((Math.random() * randomArray.length));
-		var num = randomArray[i]
-		return num
+		var num = randomArray[i];
+		return num;
 	}
 	
 	
@@ -194,7 +162,6 @@ xmlhttp.onreadystatechange = function() {
 		});
 	}
 	
-	
 	resetButton.addEventListener("click", function(){
 		console.log(imagesDisplayed)
 		randomArray = random(imageNumber,imagesDisplayed)
@@ -204,39 +171,21 @@ xmlhttp.onreadystatechange = function() {
 			if(i < imagesDisplayed){
 				var number = randomArray[i];
 				displayedFlags.push(number);
-				var a = "url("+ images[number] +")";
-				square[i].style.backgroundImage = a;
+				square[i].style.backgroundImage = "url("+ images[number] +")";
 				resultDisplay.textContent = "";
 			} else{
 				square[i].style.display = "none";
 			}					
 		}
 		num = pickNumber(displayedFlags);
-		lyricsDisplay.textContent = lyrics[num];
-	
+		countryNameDisplay.textContent = countryNames[num];
 	});	
-	  }
-	};
+  }
+}
+
 xmlhttp.open("GET", "flags.json", true);
 xmlhttp.send();
 
-
-
-// First step is to try and get every image in the array from the folder images
-// JSON File laden 
-// {
-//   "Andorra": "\'ad.png\'",
-//   "United Arab Emirates": "\'ae.png\'",
-//   "Afghanistan": "\'af.png\'",
-//   ...
-//   ...
-//   ...
-// }
-
-// var images = ['\'ad.png\'', '\'ae.png\'', '\'af.png\'',
-//  '\'ag.png\'', '\'al.png\'', '\'hu.png\'', '\'cf.png\'',
-//   '\'dj.png\'', '\'st.png\'', '\'to.png\'', '\'uz.png\'',
-//   '\'fr.png\''];
 
 
 
